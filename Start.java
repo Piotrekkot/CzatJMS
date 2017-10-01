8df67ac0d0b2ca85f5cf0b7d9cc1034c75536bcd
Piotrekkot <piotrek.kotsmc@gmail.com>
2017-10-01 22:35:53 +0200

    Initial Commit

---
+++ Start.java
@@ -0,0 +1,12 @@
+package zad12;
+
+public class Start {
+
+	public static void main(String[] args) {
+		Main lala = new Main();
+		Main lulu = new Main();
+		lala.frmCzatjms.setVisible(true);
+		lulu.frmCzatjms.setVisible(true);
+	}
+
+}
